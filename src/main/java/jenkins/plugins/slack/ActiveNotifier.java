968f8ae7568d3b4bd731ec5bd583a164c860425c
Juliana Gabos <jgabos@gumtree.com>
2015-07-22 13:03:07 +0100

    adding time metrics and adding logic to send slack notifications in separate thread

--- src/main/java/jenkins/plugins/slack/ActiveNotifier.java
+++ src/main/java/jenkins/plugins/slack/ActiveNotifier.java
@@ -27,6 +27,10 @@ import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -41,10 +45,27 @@ public class ActiveNotifier implements FineGrainedNotifier {
     SlackNotifier notifier;
     BuildListener listener;
 
+    private static ExecutorService executorService =
+            new ThreadPoolExecutor(10, 10, 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(20));
+
     public ActiveNotifier(SlackNotifier notifier, BuildListener listener) {
         super();
         this.notifier = notifier;
         this.listener = listener;
+    }
+
+    public void started(AbstractBuild build) {
+        executorService.execute(new StartedTask(build));
+    }
+
+    public void completed(AbstractBuild build) {
+        executorService.execute(new CompletedTask(build));
+    }
+
+    public void deleted(AbstractBuild r) {
+    }
+
+    public void finalized(AbstractBuild r) {
     }
 
     private SlackService getSlack(AbstractBuild r) {
@@ -67,33 +88,6 @@ public class ActiveNotifier implements FineGrainedNotifier {
         return notifier.newSlackService(teamDomain, token, projectRoom);
     }
 
-    public void deleted(AbstractBuild r) {
-    }
-
-    public void started(AbstractBuild build) {
-        
-        AbstractProject<?, ?> project = build.getProject();
-        SlackNotifier.SlackJobProperty jobProperty = project.getProperty(SlackNotifier.SlackJobProperty.class);
-
-        CauseAction causeAction = build.getAction(CauseAction.class);
-
-        if (causeAction != null) {
-            Cause scmCause = causeAction.findCause(SCMTrigger.SCMTriggerCause.class);
-            if (scmCause == null) {
-                MessageBuilder message = new MessageBuilder(notifier, build, true);
-                message.append(causeAction.getShortDescription());
-                notifyStart(build, message.appendOpenLink().toString());
-            }
-        }
-
-        String changes = getChanges(build);
-        if (changes != null) {
-            notifyStart(build, changes);
-        } else {
-            notifyStart(build, getBuildStatusMessage(build, false, jobProperty.includeCustomMessage()));
-        }
-    }
-
     private void notifyStart(AbstractBuild build, String message) {
         AbstractProject<?, ?> project = build.getProject();
         AbstractBuild<?, ?> previousBuild = project.getLastBuild().getPreviousCompletedBuild();
@@ -101,41 +95,6 @@ public class ActiveNotifier implements FineGrainedNotifier {
             getSlack(build).publish(message, "good");
         } else {
             getSlack(build).publish(message, getBuildColor(previousBuild));
-        }
-    }
-
-    public void finalized(AbstractBuild r) {
-    }
-
-    public void completed(AbstractBuild r) {
-        AbstractProject<?, ?> project = r.getProject();
-        SlackNotifier.SlackJobProperty jobProperty = project.getProperty(SlackNotifier.SlackJobProperty.class);
-        if (jobProperty == null) {
-            logger.warning("Project " + project.getName() + " has no Slack configuration.");
-            return;
-        }
-        Result result = r.getResult();
-        AbstractBuild<?, ?> previousBuild = project.getLastBuild();
-        do {
-            previousBuild = previousBuild.getPreviousCompletedBuild();
-        } while (previousBuild != null && previousBuild.getResult() == Result.ABORTED);
-        Result previousResult = (previousBuild != null) ? previousBuild.getResult() : Result.SUCCESS;
-        if ((result == Result.ABORTED && jobProperty.getNotifyAborted())
-                || (result == Result.FAILURE
-                && (previousResult != Result.FAILURE || jobProperty.getNotifyRepeatedFailure())
-                && jobProperty.getNotifyFailure())
-                || (result == Result.NOT_BUILT && jobProperty.getNotifyNotBuilt())
-                || (result == Result.SUCCESS
-                && (previousResult == Result.FAILURE || previousResult == Result.UNSTABLE)
-                && jobProperty.getNotifyBackToNormal())
-                || (result == Result.SUCCESS && jobProperty.getNotifySuccess())
-                || (result == Result.UNSTABLE && jobProperty.getNotifyUnstable())) {
-            getSlack(r).publish(
-                    getBuildStatusMessage(r, jobProperty.includeTestSummary(), jobProperty.includeCustomMessage()),
-                    getBuildColor(r));
-            if (jobProperty.getShowCommitList()) {
-                getSlack(r).publish(getCommitList(r), getBuildColor(r));
-            }
         }
     }
 
@@ -479,6 +438,96 @@ public class ActiveNotifier implements FineGrainedNotifier {
 
         public String toString() {
             return message.toString();
+        }
+    }
+
+    private class StartedTask implements Runnable {
+        private AbstractBuild build;
+
+        public StartedTask(AbstractBuild build) {
+            this.build = build;
+        }
+
+        public void run() {
+            listener.getLogger().println("going to send slack notification in separate thread (pls don't disable me big T, it me small J)");
+
+            long startTime = System.currentTimeMillis();
+
+            AbstractProject<?, ?> project = build.getProject();
+            SlackNotifier.SlackJobProperty jobProperty = project.getProperty(SlackNotifier.SlackJobProperty.class);
+
+            CauseAction causeAction = build.getAction(CauseAction.class);
+
+            if (causeAction != null) {
+                Cause scmCause = causeAction.findCause(SCMTrigger.SCMTriggerCause.class);
+                if (scmCause == null) {
+                    MessageBuilder message = new MessageBuilder(notifier, build, true);
+                    message.append(causeAction.getShortDescription());
+                    notifyStart(build, message.appendOpenLink().toString());
+                }
+            }
+
+            String changes = getChanges(build);
+            if (changes != null) {
+                notifyStart(build, changes);
+            } else {
+                notifyStart(build, getBuildStatusMessage(build, false, jobProperty.includeCustomMessage()));
+            }
+
+            long stopTime = System.currentTimeMillis();
+            long elapsedTime = stopTime - startTime;
+
+            listener.getLogger().println("Sending Slack notification took: " + elapsedTime + "ms at " + build.getFullDisplayName());
+
+        }
+    }
+
+    private class CompletedTask implements Runnable {
+        private AbstractBuild build;
+
+        public CompletedTask(AbstractBuild build) {
+            this.build = build;
+        }
+
+        public void run() {
+
+            long startTime = System.currentTimeMillis();
+
+            AbstractProject<?, ?> project = build.getProject();
+            SlackNotifier.SlackJobProperty jobProperty = project.getProperty(SlackNotifier.SlackJobProperty.class);
+            if (jobProperty == null) {
+                logger.warning("Project " + project.getName() + " has no Slack configuration.");
+                return;
+            }
+            Result result = build.getResult();
+            AbstractBuild<?, ?> previousBuild = project.getLastBuild();
+            do {
+                previousBuild = previousBuild.getPreviousCompletedBuild();
+            } while (previousBuild != null && previousBuild.getResult() == Result.ABORTED);
+            Result previousResult = (previousBuild != null) ? previousBuild.getResult() : Result.SUCCESS;
+            if ((result == Result.ABORTED && jobProperty.getNotifyAborted())
+                    || (result == Result.FAILURE
+                    && (previousResult != Result.FAILURE || jobProperty.getNotifyRepeatedFailure())
+                    && jobProperty.getNotifyFailure())
+                    || (result == Result.NOT_BUILT && jobProperty.getNotifyNotBuilt())
+                    || (result == Result.SUCCESS
+                    && (previousResult == Result.FAILURE || previousResult == Result.UNSTABLE)
+                    && jobProperty.getNotifyBackToNormal())
+                    || (result == Result.SUCCESS && jobProperty.getNotifySuccess())
+                    || (result == Result.UNSTABLE && jobProperty.getNotifyUnstable())) {
+                getSlack(build).publish(
+                        getBuildStatusMessage(build, jobProperty.includeTestSummary(), jobProperty.includeCustomMessage()),
+                        getBuildColor(build));
+                if (jobProperty.getShowCommitList()) {
+                    getSlack(build).publish(getCommitList(build), getBuildColor(build));
+                }
+            }
+
+            long stopTime = System.currentTimeMillis();
+            long elapsedTime = stopTime - startTime;
+
+            listener.getLogger().println("Sending Slack notification took: " + elapsedTime + "ms at " + build.getFullDisplayName());
+            logger.info("Sending Slack notification took: " + elapsedTime + "ms at " + build.getFullDisplayName());
         }
     }
 }
